bd621388b30c6895a884dd367eb4084ae008803e
akhila2akhila <akhila2akhila@gmail.com>
2020-05-15 13:38:18 -0400

    Initial Commit

---
+++ emoji/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  emoji
+//
+//  Created by Devi Krishna Lanka on 5/15/20.
+//  Copyright © 2020 udemy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
